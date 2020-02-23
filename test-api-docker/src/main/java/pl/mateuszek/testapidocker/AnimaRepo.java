3598d4464973d7620463db1ed6fb55470d2975d8
Mateusz <whiteelk@wp.pl>
2020-02-23 16:43:46 +0100

    WORKING FINE!

--- test-api-docker/src/main/java/pl/mateuszek/testapidocker/AnimaRepo.java
+++ test-api-docker/src/main/java/pl/mateuszek/testapidocker/AnimaRepo.java
@@ -1,7 +1,9 @@
 package pl.mateuszek.testapidocker;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 interface AnimaRepo extends CrudRepository<Animal, Long> {
     /* tutaj będzie extend CrudReposiotory*/
 }
